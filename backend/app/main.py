d295072ccecd4376032b209b6b0503cfe41e6cf2
HARSHITHA007-SPEC <harshugitlab56@gmail.com>
2026-01-09 10:52:27 +0530

    cors policy

--- backend/app/main.py
+++ backend/app/main.py
@@ -6,11 +6,26 @@ from fastapi import FastAPI, Depends
 from sqlalchemy.orm import Session
 from app.database import get_db
 from app import models, schemas
+from fastapi.middleware.cors import CORSMiddleware
+
 
 
 models.Base.metadata.create_all(bind=engine)
 
 app = FastAPI(title="Employee Login System")
+
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=[
+        "http://localhost:3000",   # React
+        "http://127.0.0.1:3000"
+    ],
+    allow_credentials=True,
+    allow_methods=["*"],  # GET, POST, PUT, DELETE
+    allow_headers=["*"],
+)
+
 
 app.include_router(auth_router, prefix="/auth")
 
